7eac10e28376a439aeed21fa93b0ee401238a7e2
ZheXian <84336243+oncelxlc@users.noreply.github.com>
2026-02-12 18:10:25 +0800

    chore: Initial commit from Create Next App

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.scss';
+import React from 'react';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'ZheXian',
+  description: '自有项目学习与记录',
+};
+
+export default function RootLayout({children}: Readonly<{ children: React.ReactNode; }>) {
+  return (
+    <html lang="zh_cn">
+    <body className={ `${ geistSans.variable } ${ geistMono.variable }` }>
+    { children }
+    </body>
+    </html>
+  );
+}
